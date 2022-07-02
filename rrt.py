c7708a358eabd19f7200462abdd320546c756b23
vikrams169 <vikramsetty169@gmail.com>
2022-07-02 14:24:39 +0530

    some more changes

--- rrt.py
+++ rrt.py
@@ -11,6 +11,14 @@ EPISOLON = 5
 OBSTACLES = [{"rectangles":[(300,700,150,600),(700,500,250,100)],"circles":[(850,850,100)]},{"rectangles":[(700,850,100,900)],"circles"=[(350,350,200),(900,700,50)]}]
 MAP_TYPE = 0
 
+# Defining colour values across the RGB Scale
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
+ORANGE = (255,164.5,0)
+
 # Initializing the grid world as a pygame display window,
 pygame.display.set_caption('RRT Path Finding Algorithm Visualization')
 viz_window = pygame.display.set_mode((WINDOW_LENGTH,WINDOW_BREADTH))
@@ -25,30 +33,17 @@ class Node:
 		self.target_node = target_node
 
 	def visualize_node(self,viz_window):
-		colour = BLUE
+		colour = RED
 		if self.start_node or self.target_node:
 			colour = ORANGE
 		pygame.draw.circle(viz_window,colour,(self.x,self.y),NODE_RADIUS,width=0)
 
-def circle_collision(point,circle_centre,radius):
-	point_x, point_y = point
-	circle_centre_x, circle_centre_y = circle_centre
-	if math.sqrt((point_x-circle_centre_x)**2+(point_y-circle_centre_y)) < radius:
+def target_reached(node,goal):
+	node_x, node_y = node.x, node.y
+	goal_x, goal_y = goal.x, goal.y
+	if math.sqrt((node_x-goal_x)**2+(node_y-goal_y)) < GOAL_RADIUS:
 		return True
 	return False
-
-def target_reached(point,goal):
-	point_x, point_y = point
-	goal_x, goal_y = goal
-	if math.sqrt((point_x-goal_x)**2+(point_y-goal_y)) < GOAL_RADIUS:
-		return True
-	return False
-
-def make_goal_node(current_node_coord,goal_node_coord,prev_node):
-	current_node = Node(current_node_coord[0],current_node_coord[1])
-	goal_node = Node(goal_node_coord[0],goal_node_coord[1],False,True)
-	current_node.parent = prev_node
-	goal_node.parent = current_node
 
 def obstacle_collision(point,obstacle_list):
 	for rect in obstacle_list["rectangles"]:
@@ -59,7 +54,7 @@ def obstacle_collision(point,obstacle_list):
 			return True
 	return False
 
-def add_new_node(node_list,obstacle_list):
+def add_new_node(viz_window,node_list,obstacle_list):
 	while True:
 		point = (random.random()*WINDOW_LENGTH,random.random()*WINDOW_BREADTH)//1
 		nearest_node_dist = 1e7
@@ -75,6 +70,7 @@ def add_new_node(node_list,obstacle_list):
 			new_node = Node(new_pos,False,False)
 			new_node.parent = nearest_node
 			node_list.append(new_node)
+			pygame.draw.line(viz_window,BLUE,(new_node.x,new_node.y),(new_node.parent.x,new_node.parent.y))
 			return new_node,node_list
 
 def initialize_obstacles(viz_window):
@@ -94,8 +90,46 @@ def add_start_and_goal(viz_window,start_point,target_point):
 	target_node.visualize_node()
 	return start_node, target_node
 
-def rrt_algorithm(viz_window,start_pos,goal_pos,obstacle_list):
-	pass
+def display_final_path(viz_window,goal_node):
+	current_node = goal_node
+	while not current_node.start_node:
+		pygame.draw.line(viz_window,GREEN,(current_node.x,current_node.y),(current_node.parent.x,current_node.parent.y))
+
+def rrt_algorithm(viz_window,start_node,goal_node,obstacle_list):
+	node_list = []
+	node_list.add(start_node)
+	while True:
+		new_node, node_list = add_new_node(viz_window,node_list,obstacle_list)
+		if target_reached(new_node,goal_node):
+			goal_node.parent = new_node
+			nodes_list.add(goal_node)
+			break
+	display_final_path(viz_window,goal_node)
+	pygame.display.update()
+
+execute = True
+start_node_found, target_node_found = False, False
+start_pos, goal_pos = None, None
+start_node, goal_node = None, None
+obstacle_list = initiaize_obstacles(viz_window)
+while execute:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			execute = False
+		elif pygame.mouse.get_pressed()[0]:
+			pos = pygame.mouse.get_pos()
+			if not start_node_found:
+				start_pos = pos
+			elif not target_node_found:
+				target_pos = pos
+				start_node, goal_node = add_start_and_goal(viz_window,start_pos,target_pos)
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE and start_node and target_node:
+				rrt_algorithm(viz_window,start_node,target_node)
+			if event.key == pygame.K_c:
+				start_node = None
+				goal_node_node = None
+
 
 
 
